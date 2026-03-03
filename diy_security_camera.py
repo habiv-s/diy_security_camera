d9f4b9017b46dcbcbc1db42f4020b2ff85dcf1a8
Kryzle <kryzlesayat@gmail.com>
2026-03-04 04:38:45 +0800

    added motion detection w/ frame differencing and thresholding

--- diy_security_camera.py
+++ diy_security_camera.py
@@ -4,7 +4,9 @@ while True:
     _,im1 = webcam.read()
     _,im2 = webcam.read()
     diff = cv2.absdiff(im1,im2)
-    cv2.imshow("Security Camera", diff)
+    gray = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
+    _,thresh = cv2.threshold(gray,20,255,cv2.THRESH_BINARY)
+    cv2.imshow("Security Camera", thresh)
     if cv2.waitKey(10) == 27:
         break
 webcam.release()
