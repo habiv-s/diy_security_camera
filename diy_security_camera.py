299817e092ede2753d8911896664461e86a9c20c
Kryzle <kryzlesayat@gmail.com>
2026-03-04 04:51:10 +0800

    apply snake_case naming convention as per python coding standard

--- diy_security_camera.py
+++ diy_security_camera.py
@@ -1,19 +1,25 @@
 import cv2
 import winsound
+
 webcam = cv2.VideoCapture(0)
+
 while True:
-    _,im1 = webcam.read()
-    _,im2 = webcam.read()
-    diff = cv2.absdiff(im1,im2)
-    gray = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
-    _,thresh = cv2.threshold(gray,20,255,cv2.THRESH_BINARY)
-    contours,_ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    for c in contours:
-        if cv2.contourArea(c) < 5000:
+    _, image_one = webcam.read()
+    _, image_two = webcam.read()
+    frame_diff = cv2.absdiff(image_one,image_two)
+    gray_diff = cv2.cvtColor(frame_diff,cv2.COLOR_BGR2GRAY)
+    _, thresh_image = cv2.threshold(gray_diff,20,255,cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(thresh_image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
             continue
         winsound.Beep(500,100)
-    cv2.imshow("Security Camera", thresh)
+
+    cv2.imshow("Security Camera", thresh_image)
+
     if cv2.waitKey(10) == 27:
         break
+
 webcam.release()
 cv2.destroyAllWindows()
